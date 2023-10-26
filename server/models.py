d33bd9443be500992611fb556fb51f501507e36b
Kiarie <collinskiarie13@outlook.com>
2023-10-26 15:19:04 +0300

    Create the Recipe, Comment, Favourite and Rating models

--- server/models.py
+++ server/models.py
@@ -19,3 +19,50 @@ class User(BaseModel):
     password = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     created_at = Column(DateTime, onupdate=func.now())
+
+
+class Recipe(BaseModel):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    recipe_image = Column(String, nullable = False)
+    ingredients =  Column(String, nullable = False)
+    procedure = Column(String, nullable = False)
+    number_of_people_served = Column(Integer, nullable = False)
+    time_in_minutes = Column(Integer, nullable = False)
+    user_id = Column(Integer, nullable = False)
+    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, onupdate=func.now())
+
+class Comment(BaseModel):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key = True)
+    comment = Column(String, nullable = False)
+    user_id = Column(Integer, nullable = False)
+    recipe_id = Column(Integer, nullable = False)
+    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, onupdate=func.now())
+
+
+class Favourites(BaseModel):
+    __tablename__ = "favourites"
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = False)
+    recipe_id = Column(Integer, nullable = False)
+    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, onupdate=func.now())
+
+class Rating(BaseModel):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    comment = Column(String, nullable = False)
+    user_id = Column(Integer, nullable = False)
+    recipe_id = Column(Integer, nullable = False)
+    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, onupdate=func.now())
+
+
