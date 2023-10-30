54e367303456880516ef7d476c80653d3bd6250e
Kiarie <collinskiarie13@outlook.com>
2023-10-30 23:17:41 +0300

    Added the update route for users

--- server/routes/users.py
+++ server/routes/users.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from fastapi import APIRouter, Depends, status, HTTPException
 
-from ..schemas import ShowUserSchema
+from ..schemas import ShowUserSchema, UpdateUserSchema
 from ..repository.user import UserRepository
 from ..database import get_db
 from ..auth.auth_bearer import JWTBearer
@@ -31,3 +31,19 @@ async def get_user_by_id(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
         )
     return user
+
+@router.put(
+    "/{id}",
+    response_model=ShowUserSchema,
+    dependencies=[Depends(JWTBearer())],
+)
+async def update_user(id: int, user_data: UpdateUserSchema, db: Session = Depends(get_db)):
+    existing_user = UserRepository.get_user_by_id(id, db)
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+        )
+
+    updated_user = UserRepository.update_user(existing_user, user_data, db)
+    return updated_user
