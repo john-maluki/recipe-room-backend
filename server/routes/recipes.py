6a7bd92a9d43bd979afb0d74c4bd000b6cd0865c
Kiarie <collinskiarie13@outlook.com>
2023-10-30 22:11:27 +0300

    Created the Put route

--- server/routes/recipes.py
+++ server/routes/recipes.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from fastapi import APIRouter, Depends, status, HTTPException
 
-from ..schemas import RecipeSchema, CreateRecipeSchema
+from ..schemas import RecipeSchema, CreateRecipeSchema, UpdateRecipeSchema
 from ..database import get_db
 from ..repository.recipe import RecipeRepository
 from ..auth.auth_bearer import JWTBearer
@@ -35,3 +35,14 @@ async def create_recipe(
 ):
     created_recipe = RecipeRepository.create_recipe(db, recipe_data)
     return created_recipe
+
+@router.put("/{id}", response_model=RecipeSchema, dependencies=[Depends(JWTBearer())])
+async def update_recipe(id: int, recipe_data: UpdateRecipeSchema, db: Session = Depends(get_db)):
+    existing_recipe = RecipeRepository.get_recipe_by_id(id, db)
+    if not existing_recipe:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Recipe not found!"
+        )
+    
+    updated_recipe = RecipeRepository.update_recipe(db, existing_recipe, recipe_data)
+    return updated_recipe
