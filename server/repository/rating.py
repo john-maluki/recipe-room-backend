e461fa861bc96e0bdf5922cb31a9c8f571c826bc
Kiarie <collinskiarie13@outlook.com>
2023-11-02 21:45:56 +0300

    Created the RatingRepository and added a create_rating method

---
+++ server/repository/rating.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session
+from fastapi import status, HTTPException
+from ..models import Rating, User
+from ..schemas import CreateRatingSchema
+
+class RatingRepository:
+    def create_rating(db: Session, rating_data: CreateRatingSchema):
+        user_id = rating_data.user_id
+
+        user = db.query(User).filter(User.id == user_id).first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+            )
+        
+        new_rating = Rating(
+            rating = rating_data.rating,
+            user_id=rating_data.user_id,
+            recipe_id=rating_data.recipe_id
+
+        )
+        db.add(new_rating)
+        db.commit()
+        db.refresh(new_rating)
+        return new_rating
+    
+    
