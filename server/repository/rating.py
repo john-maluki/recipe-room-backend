573c5d287bed76cef9f583f76743900e005fe034
Kiarie <collinskiarie13@outlook.com>
2023-11-02 23:08:57 +0300

    Created the get and update methods

--- server/repository/rating.py
+++ server/repository/rating.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import status, HTTPException
 from ..models import Rating, User
-from ..schemas import CreateRatingSchema
+from ..schemas import CreateRatingSchema, UpdateRatingSchema
 from typing import List
 
 class RatingRepository:
@@ -28,3 +28,25 @@ class RatingRepository:
     def get_ratings_by_recipe_id(db: Session, recipe_id: int) -> List[Rating]:
         return db.query(Rating).filter(Rating.recipe_id == recipe_id).all()
     
+    def get_ratings_by_user_id(db: Session, user_id: int):
+        user = db.query(User).filter(User.id == user_id).first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+            )
+        return db.query(Rating).filter(Rating.user_id == user_id).all()
+    
+    def get_rating_by_id(db: Session, rating_id: int):
+        return db.query(Rating).filter(Rating.id == rating_id).first()
+    
+    def update_rating(db: Session, rating_id: int, updated_data: UpdateRatingSchema):
+        existing_rating = db.query(Rating).filter(Rating.id == rating_id).first()
+
+        if existing_rating:
+            for field, value in updated_data.dict().items():
+                setattr(existing_rating, field, value)
+
+            db.commit()
+            db.refresh(existing_rating)
+
+        return existing_rating
