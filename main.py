3efd666c0a26f90e20abe89e18783fd57b1c166b
john <johnmaluki58@gmail.com>
2023-11-09 19:01:18 +0300

    Updatebrequirements

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+from uvicorn import run
+
 from server import app
-from server.routes import recipes, users, index, auths, comments, favourite_recipes, ratings
+from server.routes import (
+    recipes,
+    users,
+    index,
+    auths,
+    comments,
+    favourite_recipes,
+    ratings,
+)
 
 app.include_router(index.router)
 app.include_router(auths.router)
@@ -8,3 +19,8 @@ app.include_router(recipes.router)
 app.include_router(comments.router)
 app.include_router(favourite_recipes.router)
 app.include_router(ratings.router)
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = int(os.environ.get("PORT", 8000))
+    run("main:app", host=host, port=port)
